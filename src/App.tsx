3f76259b4cfc5fba527818e27330f1da74a46c0f
baramee <baramee.papim@gmail.com>
2025-12-25 16:01:56 +0700

    build: initialize React project with Vite and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <section>
+        <h1 className="text-3xl font-bold underline">My First React App on Vite</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
